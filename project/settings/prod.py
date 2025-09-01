bb53e6f8df7ce7e32db89d5466143ae11d1ddbce
Stepan <stevedekart2020@gmail.com>
2025-09-01 23:22:28 +0200

    fixed workflow

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -2,4 +2,23 @@ from .base import *
 
 os.environ["ENVIRONMENT"] = "prod"
 
-ACTIVATION_PATTERN_URL_FRONTEND = 'https://localhost:8001/<code>'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:8080",
+    # "http://127.0.0.1:8000",
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
+
+# ACTIVATION_PATTERN_URL_FRONTEND = 'https://localhost:8001/activate/<code>'
+# RESET_PASS_PATTERN_URL_FRONTEND = 'https://localhost:8001/reset/<code>'
